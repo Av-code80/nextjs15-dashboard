eddb7151ad1eb5e0197a0bafa2062502939844c4
Av-code80 <dev.avehghasemian@gmail.com>
2024-09-08 23:29:56 +0200

    feat: :sparkles: implement authentication using next-auth.js and useActionState

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,7 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
+            alt="Screenshot of the dashboard"
           />
           <Image
             src={"/hero-mobile.png"}
